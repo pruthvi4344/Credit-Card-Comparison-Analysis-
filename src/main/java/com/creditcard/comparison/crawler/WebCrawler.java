002e7a575b2cf918916013a44482784caa574f19
pruthvi4344 <pruthvirathod@gmail.com>
2026-02-06 22:45:23 -0500

    test commit

---
+++ src/main/java/com/creditcard/comparison/crawler/WebCrawler.java
@@ -0,0 +1,9 @@
+package com.creditcard.comparison.crawler;
+
+public class WebCrawler {
+
+    public void startCrawling() {
+        System.out.println("Crawling websites...");
+        // TODO: Crawl credit card websites
+    }
+}
